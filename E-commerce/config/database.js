042b2e567575ac1596986ceb713f99878511abd9
komalshivani <komalshivani2@gmail.com>
2019-06-28 23:38:35 -0400

    changes made in usercollection

--- E-commerce/config/database.js
+++ E-commerce/config/database.js
@@ -1,13 +1,13 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-mongoose.Promise=global.Promise
+mongoose.Promise = global.Promise
 
-mongoose.connect(`mongodb://localhost:27017/e-commerce-app`,{ useNewUrlParser: true })
-.then(res=>{
-    console.log('connecte to db successfully')
-})
-.catch(err=>{
-    console.log('err while connecting to db')
-})
+mongoose.connect(`mongodb://localhost:27017/e-commerce-app`, { useNewUrlParser: true })
+    .then(res => {
+        console.log('connecte to db successfully')
+    })
+    .catch(err => {
+        console.log('err while connecting to db')
+    })
 
-module.exports={mongoose}
+module.exports = { mongoose }
