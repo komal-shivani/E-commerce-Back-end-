6c8ba061091c798f7cb99522ca3ca62111567470
komalshivani <komalshivani2@gmail.com>
2019-06-29 16:53:29 -0400

    product model

---
+++ E-commerce/app/controllers/productController.js
@@ -0,0 +1,60 @@
+const express=require('express')
+const {Product}=require('../models/product')
+const router=express.Router()
+const { authenticateUser }=require('../middleware/authenticateUser')
+const { authouriesUser } = require('../middleware/adminauthorise') 
+
+
+router.get('/', authenticateUser,(req,res)=>{
+    const {user}=req
+    Product.find({
+        user:user._id
+    })
+    .then(products=>res.json(products))
+    .catch(err=>res.json(err))
+})
+
+router.post('/', authenticateUser, authouriesUser,(req,res)=>{
+    const {user}=req
+    const body=req.body
+    const product=new Product(body)
+    product.user=user._id
+    product.save()
+    .then(product=>res.json(product))
+    .catch(err=>res.json(err))
+})
+
+router.get('/:id', authenticateUser,(req,res)=>{
+    const id=req.params.id
+    product.findOne({
+        _id:id,
+        user:req.user._id
+    })
+    .then(product=>res.json(product))
+    .then(err=>res.json(err))
+})
+
+router.put('/:id', authenticateUser,(req,res)=>{
+    const id=req.params.id
+    Product.findOneAndUpdate({
+        _id:id,
+        user:req.user._id},
+        body,{new:true, runValidators:true})
+    .then(product => res.json(product))
+    .then(err => res.json(err))
+})
+
+router.delete('/:id', authenticateUser,(req,res)=>{
+    const id=req.params.id
+    Product.findByIdAndDelete({
+        _id:id,
+        user:req.user._id
+    
+    })
+    .then(product => res.json(product))
+    .then(err => res.json(err))   
+})
+
+module.exports={
+    productRouter:router
+}
