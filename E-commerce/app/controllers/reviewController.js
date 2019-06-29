3ce76ef5f9051366adc82035f96f31069652512e
komalshivani <komalshivani2@gmail.com>
2019-06-29 17:31:34 -0400

    review model

---
+++ E-commerce/app/controllers/reviewController.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const router=express.Router()
+const {Review}=require('../models/review')
+const { authouriesUser } = require('../middleware/adminauthorise')
+const { authenticateUser } = require('../middleware/authenticateUser')
+
+router.get('/', authenticateUser,(req,res)=>{
+    const {user}=req
+    Review.find({
+        user:user._id
+    })
+    .then(reviews=>res.json(reviews))
+    .catch(err=>res.json(err))
+})
+
+router.post('/', authenticateUser, authouriesUser,(req,res)=>{
+    const body=req.body
+    const {user}=req
+    const review=new Review(body)
+    review.user=user._id
+    review.save()
+    .then(review=>res.json(review))
+    .catch(err=>res.json(err))
+})
+
+router.delete('/:id', authouriesUser ,(req,res)=>{
+    const id=req.params.id
+    Review.findOneAndDelete({
+        _id:id,
+        user:user._id
+    })
+    .then(review => res.json(review))
+    .catch(err => res.json(err))
+})
+
+module.exports={
+    reviewRouter:router
+}
